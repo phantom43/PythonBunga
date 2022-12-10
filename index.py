b996fa06deaf6041bc8ce486f67f047545123f1b
rezky <88895489+phantom43@users.noreply.github.com>
2022-12-10 08:59:41 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.bgcolor("black")
+turtle.speed(0)
+turtle.hideturtle()
+
+Colors = ["yellow", "red", "yellow", "red"]
+
+for i in range (120):
+    for c in Colors:
+        turtle.color(c)
+        turtle.circle(200-i, 100)
+        turtle.lt(90)
+        turtle.circle(200-i, 100)
+        turtle.rt(60)
+        turtle.end_fill()
+turtle.mainloop()
